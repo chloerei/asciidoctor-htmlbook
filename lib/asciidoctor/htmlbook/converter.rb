b4dda912bfe4acb07221f53fcf39e526a1f52324
Rei <chloerei@gmail.com>
2017-01-15 19:49:39 +0800

    Extract node to liquid methods

--- lib/asciidoctor/htmlbook/converter.rb
+++ lib/asciidoctor/htmlbook/converter.rb
@@ -17,25 +17,69 @@ module Asciidoctor
         path = File.join DEFAULT_TEMPLATE_PATH, "#{node_name}.html"
         if File.exist?(path)
           @templates[node_name] = Liquid::Template.parse(File.read(path))
-        else
-          raise "Template not found: #{node_name}"
         end
       end
 
       def convert(node, transform = nil, options = {})
         template = get_template(node.node_name)
+
+        unless template
+          raise "Template not found #{node.node_name} #{node} #{node.attributes}"
+        end
+
         case node
         when Asciidoctor::Document
-          template.render 'node' => { 'doctitle' => node.doctitle, 'content' => node.content }
+          template.render 'node' => document_to_liquid(node)
         when Asciidoctor::Section
-          template.render 'node' => { 'title' => node.title, 'sectname' => node.sectname, 'level' => node.level, 'content' => node.content, 'id' => node.id }
-        when Asciidoctor::AbstractBlock
-          template.render 'node' => { 'title' => node.title, 'content' => node.content }
+          template.render 'node' => section_to_liquid(node)
         when Asciidoctor::Inline
-          template.render 'node' => { 'text' => node.text }
+          template.render 'node' => inline_to_liquid(node)
         else
           raise "Uncatch type"
         end
+      end
+
+      def abstract_node_to_liquid(node)
+        {
+          'context' => node.context,
+          'node_name' => node.node_name,
+          'id' => node.id,
+          'attributes' => node.attributes
+        }
+      end
+
+      def abstract_block_to_liquid(node)
+        abstract_node_to_liquid(node).merge({
+          'level' => node.level,
+          'title' => node.title,
+          'style' => node.style,
+          'caption' => node.caption,
+          'content' => node.content
+        })
+      end
+
+      def document_to_liquid(node)
+        abstract_block_to_liquid(node).merge({
+          'doctitle' => node.doctitle
+        })
+      end
+
+      def section_to_liquid(node)
+        abstract_block_to_liquid(node).merge({
+          'index' => node.index,
+          'number' => node.number,
+          'sectname' => node.sectname,
+          'special' => node.special,
+          'numbered' => node.numbered
+        })
+      end
+
+      def inline_to_liquid(node)
+        abstract_node_to_liquid(node).merge({
+          'text' => node.text,
+          'type' => node.type,
+          'target' => node.target
+        })
       end
     end
   end
