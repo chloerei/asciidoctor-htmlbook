eeb2cb0c287b75d30eb06c27c8fefad36aa27f20
Rei <chloerei@gmail.com>
2017-01-30 00:36:30 +0800

    Add image size and link support

--- test/asciidoctor/htmlbook/converter/image_test.rb
+++ test/asciidoctor/htmlbook/converter/image_test.rb
@@ -30,4 +30,20 @@ class Asciidoctor::Htmlbook::Converter::ImageTest < ConverterTest
 
     assert_convert_body html, doc
   end
+
+  def test_convert_image_with_size_and_link
+    doc = <<~EOF
+      image::http://example.com/logo.png[logo, 400, 300, link="http://example.com/"]
+    EOF
+
+    html = <<~EOF
+      <figure>
+        <a href="http://example.com/">
+          <img src="http://example.com/logo.png" alt="logo" width="400" height="300" />
+        </a>
+      </figure>
+    EOF
+
+    assert_convert_body html, doc
+  end
 end
