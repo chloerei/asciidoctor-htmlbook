b5f74c17f496e6fba768ba840171acb297a04b92
Rei <chloerei@gmail.com>
2017-01-29 10:04:50 +0800

    Add literal support

---
+++ test/asciidoctor/htmlbook/converter/literal_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class Asciidoctor::Htmlbook::Converter::LiteralTest < ConverterTest
+  def test_convert_literal
+    doc = <<~EOF
+      ....
+      Text
+      ....
+    EOF
+
+    html = <<~EOF
+      <pre>Text</pre>
+    EOF
+
+    assert_convert_equal html, doc
+  end
+end
