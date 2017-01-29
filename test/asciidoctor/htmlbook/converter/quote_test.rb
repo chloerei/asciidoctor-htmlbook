0a967559298fd65ec07d602450e32757b08c82c8
Rei <chloerei@gmail.com>
2017-01-29 11:40:16 +0800

    Add quote citetitle support

--- test/asciidoctor/htmlbook/converter/quote_test.rb
+++ test/asciidoctor/htmlbook/converter/quote_test.rb
@@ -17,24 +17,19 @@ class Asciidoctor::Htmlbook::Converter::QuoteTest < ConverterTest
     assert_convert_equal html, doc
   end
 
-  def test_convert_quote_with_attribution
+  def test_convert_quote_with_attribution_and_citetitle
     doc = <<~EOF
-      [quote, Monty Python and the Holy Grail]
+      [quote, attribution, citetitle]
       ____
-      Dennis: Come and see the violence inherent in the system. Help! Help! I'm being repressed!
-
-      King Arthur: Bloody peasant!
-
-      Dennis: Oh, what a giveaway! Did you hear that? Did you hear that, eh? That's what I'm on about! Did you see him repressing me? You saw him, Didn't you?
+      Quote content.
       ____
     EOF
 
     html = <<~EOF
       <blockquote>
-        <p>Dennis: Come and see the violence inherent in the system. Help! Help! I&#8217;m being repressed!</p>
-        <p>King Arthur: Bloody peasant!</p>
-        <p>Dennis: Oh, what a giveaway! Did you hear that? Did you hear that, eh? That&#8217;s what I&#8217;m on about! Did you see him repressing me? You saw him, Didn&#8217;t you?</p>
-        <p data-type="attribution">Monty Python and the Holy Grail</p>
+        <p>Quote content.</p>
+        <p data-type="attribution">attribution</p>
+        <p data-type="citetitle">citetitle</p>
       </blockquote>
     EOF
 
