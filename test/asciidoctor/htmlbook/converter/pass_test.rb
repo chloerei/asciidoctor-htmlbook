f6055a74f85e5a6b564a8738a637ef8a4960938e
Rei <chloerei@gmail.com>
2017-01-29 11:17:35 +0800

    Add pass support

---
+++ test/asciidoctor/htmlbook/converter/pass_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class Asciidoctor::Htmlbook::Converter::PassTest < ConverterTest
+  def test_convert_pass
+    doc = <<~EOF
+      ++++
+      Pass
+      ++++
+    EOF
+
+    html = <<~EOF
+      Pass
+    EOF
+
+    assert_convert_equal html, doc
+  end
+end
