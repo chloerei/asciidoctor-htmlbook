2f734f68c8448e0308897e2bd4c74dc555700c7d
Rei <chloerei@gmail.com>
2017-01-29 16:07:14 +0800

    Add sidebar support

---
+++ test/asciidoctor/htmlbook/converter/sidebar_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class Asciidoctor::Htmlbook::Converter::SidebarTest < ConverterTest
+  def test_convert_sidebar
+    doc = <<~EOF
+      .Sidebar Title
+      [[sidebar]]
+      ****
+      Sidebar content.
+      ****
+    EOF
+
+    html = <<~EOF
+      <aside data-type="sidebar" id="sidebar">
+        <h5>Sidebar Title</h5>
+        <p>Sidebar content.</p>
+      </aside>
+    EOF
+
+    assert_convert_body html, doc
+  end
+end
