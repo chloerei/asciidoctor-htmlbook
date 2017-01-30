1efba81503dc9dbfd287699e98372f57e64b5dc3
Rei <chloerei@gmail.com>
2017-01-30 20:08:44 +0800

    Add inline footnote support

---
+++ test/asciidoctor/htmlbook/converter/inline_footnote_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class Asciidoctor::Htmlbook::Converter::InlineFootnoteTest < ConverterTest
+  def test_convert_footnote
+    doc = <<~EOF
+      Content.footnote:[Footnote content.]
+    EOF
+
+    html = <<~EOF
+      <p>Content.<span data-type="footnote">Footnote content.</span></p>
+    EOF
+
+    assert_convert_body html, doc
+  end
+
+  def test_convert_footnoteref
+    doc = <<~EOF
+      Content one.footnoteref:[refid, Footnote content.]
+
+      Content two.footnoteref:[refid]
+    EOF
+
+    html = <<~EOF
+      <p>Content one.<span data-type="footnote">Footnote content.</span></p>
+
+      <p>Content two.<span data-type="footnote">Footnote content.</span></p>
+    EOF
+
+    assert_convert_body html, doc
+  end
+end
