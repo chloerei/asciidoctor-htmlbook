e94324387730cd970ebef30d05d6b0f09d0f1f98
Rei <chloerei@gmail.com>
2017-01-30 11:47:09 +0800

    Add video options support

--- test/asciidoctor/htmlbook/converter/video_test.rb
+++ test/asciidoctor/htmlbook/converter/video_test.rb
@@ -7,7 +7,7 @@ class Asciidoctor::Htmlbook::Converter::VideoTest < ConverterTest
     EOF
 
     html = <<~EOF
-      <video src="video_file.mp4">
+      <video src="video_file.mp4" controls="controls">
         <em>Sorry, the &lt;video&gt; element not supported in your reading system.</em>
       </video>
     EOF
@@ -21,7 +21,21 @@ class Asciidoctor::Htmlbook::Converter::VideoTest < ConverterTest
     EOF
 
     html = <<~EOF
-      <video src="video_file.mp4" width="480" height="270" poster="video_poster.png">
+      <video src="video_file.mp4" controls="controls" width="480" height="270" poster="video_poster.png">
+        <em>Sorry, the &lt;video&gt; element not supported in your reading system.</em>
+      </video>
+    EOF
+
+    assert_convert_body html, doc
+  end
+
+  def test_convert_video_with_options
+    doc = <<~EOF
+      video::video_file.mp4[options="autoplay,loop"]
+    EOF
+
+    html = <<~EOF
+      <video src="video_file.mp4" controls="controls" autoplay="autoplay" loop="loop">
         <em>Sorry, the &lt;video&gt; element not supported in your reading system.</em>
       </video>
     EOF
