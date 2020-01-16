0f235f58ad6484c927949b1121739cf4b5b4d27f
Rei <chloerei@gmail.com>
2020-01-16 16:56:38 +0800

    Fix document header unclosed

--- test/asciidoctor/htmlbook/converter/document_test.rb
+++ test/asciidoctor/htmlbook/converter/document_test.rb
@@ -42,6 +42,31 @@ class Asciidoctor::Htmlbook::Converter::DocumentTest < Minitest::Test
     assert_convert_html html, doc
   end
 
+  def test_convert_document_with_author
+    doc = <<~EOF
+      = Doc Title
+      Author
+    EOF
+
+    html = <<~EOF
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset=\"utf-8\" />
+          <title>Doc Title</title>
+        </head>
+        <body data-type="book">
+          <header>
+            <h1>Doc Title</h1>
+            <p data-type="author">Author</p>
+          </header>
+        </body>
+      </html>
+    EOF
+
+    assert_convert_html html, doc
+  end
+
   def test_convert_document_with_title_and_subtitle_and_author
     doc = <<~EOF
       = Doc Title: Subtitle
